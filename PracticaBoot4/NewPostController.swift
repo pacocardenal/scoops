bf2e4be135f6a0f14931236f582d597ae2129686
Paco Cardenal <paco.cardenal@talentomobile.com>
2017-04-08 11:33:29 +0200

    Upload post and photo

--- PracticaBoot4/NewPostController.swift
+++ PracticaBoot4/NewPostController.swift
@@ -1,14 +1,6 @@
-//
-//  NewPostController.swift
-//  PracticaBoot4
-//
-//  Created by Juan Antonio Martin Noguera on 23/03/2017.
-//  Copyright © 2017 COM. All rights reserved.
-//
-
 import UIKit
 
-class NewPostController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class NewPostController: UIViewController, UINavigationControllerDelegate {
 
     @IBOutlet weak var titlePostTxt: UITextField!
     @IBOutlet weak var textPostTxt: UITextField!
@@ -21,17 +13,18 @@ class NewPostController: UIViewController, UIImagePickerControllerDelegate, UINa
         }
     }
     
+    var client: MSClient!
+    let tableName = "noticia"
+    let azureAppServicesEndpoint = "https://boot4camplabpaco.azurewebsites.net"
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        setupAzureAppService()
     }
     
+    // MARK: - User actions
     @IBAction func takePhoto(_ sender: Any) {
         self.present(pushAlertCameraLibrary(), animated: true, completion: nil)
     }
@@ -40,23 +33,18 @@ class NewPostController: UIViewController, UIImagePickerControllerDelegate, UINa
     }
 
     @IBAction func savePostInCloud(_ sender: Any) {
-        // preparado para implementar codigo que persita en el cloud 
+        guard let postImage = imagePost.image else {
+            newPostInService(titlePostTxt.text!, textPostTxt.text!, isReadyToPublish, imgData: nil)
+            return
+        }
+        newPostInService(titlePostTxt.text!, textPostTxt.text!, isReadyToPublish, imgData: UIImageJPEGRepresentation(postImage, 0.50))
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: - funciones para la camara
+    // MARK: - Camera functions
     internal func pushAlertCameraLibrary() -> UIAlertController {
         let actionSheet = UIAlertController(title: NSLocalizedString("Selecciona la fuente de la imagen", comment: ""), message: NSLocalizedString("", comment: ""), preferredStyle: .actionSheet)
         
-        let libraryBtn = UIAlertAction(title: NSLocalizedString("Ussar la libreria", comment: ""), style: .default) { (action) in
+        let libraryBtn = UIAlertAction(title: NSLocalizedString("Usar la libreria", comment: ""), style: .default) { (action) in
             self.takePictureFromCameraOrLibrary(.photoLibrary)
             
         }
@@ -95,8 +83,11 @@ class NewPostController: UIViewController, UIImagePickerControllerDelegate, UINa
 
 }
 
-// MARK: - Delegado del imagepicker
-extension NewPostController {
+// MARK: - Delegates
+
+// UIImagePickerControllerDelegate
+extension NewPostController: UIImagePickerControllerDelegate {
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         imageCaptured = (info[UIImagePickerControllerOriginalImage] as? UIImage)!
         self.dismiss(animated: false, completion: {
@@ -105,14 +96,54 @@ extension NewPostController {
     
 }
 
-
-
-
-
-
-
-
-
-
-
-
+// MARK: - Appservice methods
+extension NewPostController {
+    
+    func setupAzureAppService() {
+        client = MSClient(applicationURLString: azureAppServicesEndpoint)
+    }
+    
+    func uploadDataPost(data: Data, completionHandler: @escaping (String?) -> Void) {
+        // Credenciales
+        let credential = AZSStorageCredentials(accountName: "pacoboot4", accountKey: "/XJTFKPk/hgjK7AAnUreXXqUFOqmM3BHLgbrIOI1IRUIvqFFRK+ujVzRUxVI8qGX0+lWLRupa64KUUNA/ZBjyg==")
+        
+        do {
+            let account = try AZSCloudStorageAccount(credentials: credential, useHttps: true)
+            let blobClient = account.getBlobClient()
+            let container = blobClient?.containerReference(fromName: "fotos")
+            let blobBlock = container?.blockBlobReference(fromName: String("\(UUID().uuidString).jpg"))
+            blobBlock?.upload(from: data, completionHandler: { (error) in
+                if error == nil {
+                    completionHandler(blobBlock?.blobName)
+                } else {
+                    completionHandler(nil)
+                }
+            })
+        } catch {
+            
+        }
+    }
+    
+    func newPostInService(_ title: String, _ description: String, _ status: Bool, imgData: Data?) {
+        let posts = client.table(withName: tableName)
+        
+        posts.insert([ "titulo" : title, "texto" : description, "publicada" : status ]) { (result, error) in
+            if let _ = error {
+                print("\(error)")
+                return
+            }
+            if let _ = imgData {
+                self.uploadDataPost(data: imgData!, completionHandler: { (blobname) in
+                    let item = [ "id" : (result?["id"] as! String), "foto" : blobname ]
+                    posts.update(item, completion: { (result, error) in
+                        if let _ = error {
+                            print("\(error?.localizedDescription)")
+                        }
+                        print("\(result)")
+                    })
+                })
+            }
+        }
+    }
+    
+}

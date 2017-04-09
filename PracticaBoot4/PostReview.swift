de5b95ab383871b33bbdb9ec7dd50230c3eee842
Paco Cardenal <paco.cardenal@talentomobile.com>
2017-04-09 11:17:19 +0200

    Sync model with view

--- PracticaBoot4/PostReview.swift
+++ PracticaBoot4/PostReview.swift
@@ -15,6 +15,7 @@ class PostReview: UIViewController {
     @IBOutlet weak var postTxt: UITextField!
     @IBOutlet weak var titleTxt: UITextField!
     @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var averageLabel: UILabel!
     
     var model: Dictionary<String, Any>!
     let stepValue: Float = 1
@@ -52,6 +53,7 @@ class PostReview: UIViewController {
                 return
             }
             print(result!)
+            self.navigationController?.popViewController(animated: true)
         }
     }
 
@@ -61,8 +63,60 @@ class PostReview: UIViewController {
         titleTxt.text = (model["titulo"] as! String)
         postTxt.text = (model["texto"] as! String)
         //imagePost.image = UIImage(contentsOfFile: (model["foto"] as! String))
+        syncAverage()
+        syncPhoto()
     }
     
+    func syncAverage() {
+        let noticiaId = (model["id"] as! String)
+        let paramsToCloud = ["noticia" : noticiaId]
+        let client = MSClient(applicationURLString: "https://boot4camplabpaco.azurewebsites.net")
+        //let reviews = client.table(withName: "valoracion")
+        
+        client.invokeAPI("GetReviewsAverage", body: nil, httpMethod: "GET", parameters: paramsToCloud, headers: nil) { (result, response, error) in
+            if let _ = error {
+                print("\(error?.localizedDescription)")
+            }
+            print("\(result)")
+            var model: [Any] = []
+            model = result as! [Any]
+            let averageModel = model[0] as! [String : AnyObject]
+            
+            if let average = averageModel["media"] as? NSNumber {
+                let averageDouble = Double(average)
+                self.averageLabel.text = "Avg.: \(String(format: "%.2f", averageDouble))"
+            }
+            
+        }
+    }
     
+    func syncPhoto() {
+        let baseUrlString = "https://pacoboot4.blob.core.windows.net/fotos/"
+        if let urlFotoString = (model["foto"] as? String) {
+            let linkUrlString = baseUrlString + urlFotoString
+            imagePost.downloadedFrom(link: linkUrlString)
+        }
+    }
 
+}
+
+extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { () -> Void in
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
 }
